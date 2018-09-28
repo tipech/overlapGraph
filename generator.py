bd2ebe64f8efc2d63ca3d25cd29d6eeaef49e1e8
Tilemachos Pechlivanoglou <tipech@eecs.yorku.ca>
2018-09-28 15:27:57 -0400

    improved command line argument parsing, implemented drawing generated intervals

--- generator.py
+++ generator.py
@@ -10,7 +10,7 @@ Classes:
   IntervalGenerator -- handles generation of interval objects
 
 """
-import sys, random
+import sys, random, argparse
 from shapes import IntervalObject, RectangleObject
 from setuptools import setup
 from graphics import *
@@ -21,58 +21,51 @@ from pprint import pprint
 def main():
     """Run the generator with command-line parameters."""
 
-    try:
-        if "-h" in sys.argv or "--help" in sys.argv: # help argument
-            raise ValueError("")
+    # parser for command line arguments
+    parser = argparse.ArgumentParser(
+        description="Randomly generate overlapping objects in space.")
 
-        if "-g" in sys.argv: # graphics argument
-            sys.argv.remove("-g")
-            graphics = True
-        elif "--graphics" in sys.argv: 
-            sys.argv.remove("--graphics")
-            graphics = True
+    # positional arguments: object type, number of objects
+    parser.add_argument("type", choices=["interval", "rectangle"],
+        help="type of overlapping objects")
+    parser.add_argument("number", metavar="N", type=int,
+        help="number of generated objects")
 
-        if len(sys.argv) < 3 or len(sys.argv) > 6: # too few/many arguments
-            raise ValueError("Incorrect usage!\n")
+    # optional arguments: print results, draw results with graphics
+    parser.add_argument("-p", "--print", action="store_true",
+        help="print results to console")
+    parser.add_argument("-g", "--graphics", action="store_true",
+        help="display graphics and draw results")
+    
+    # extra optional arguments: area size, min & max object size ratios
+    parser.add_argument("--area", type=int, default=1000,
+        help="size of generated area (default: 1000)")
+    parser.add_argument("--min", type=float, default=0.01,
+        help="min ratio of object size relative to area (def: 0.01)")
+    parser.add_argument("--max", type=float, default=0.1,
+        help="max ratio of object size relative to area (def: 0.1)")
 
-        object_number = int(sys.argv[2])    # number of generated objects
+    # check for invalid numbers
+    args = parser.parse_args()
+    if args.number < 0 or args.area < 0 or args.min < 0 or args.max < 0 :
+        parser.error("Invalid number argument.")
+    if args.max > 1 or args.min > args.max :
+        parser.error("Invalid ratios.")
 
-        # size of generated area
-        area_size = int(sys.argv[3]) if len(sys.argv) > 3 else 1000
-        # min object size/area ratio
-        min_ratio = float(sys.argv[4]) if len(sys.argv) > 4 else 0.01
-        # max object size/area ratio
-        max_ratio = float(sys.argv[5]) if len(sys.argv) > 5 else 0.1
-
-        # Type of overlap problem
-        if sys.argv[1] == "rectangle":
-            generator = RectangleGenerator(object_number, area_size,
-                min_ratio, max_ratio)
-        elif sys.argv[1] == "interval":
-            generator = IntervalGenerator(object_number, area_size,
-                min_ratio, max_ratio)
-        else: # unknown type
-            raise ValueError("Incorrect object type!\n")
-
+    # run generator
+    if args.type == "interval":
+        generator = IntervalGenerator(args.number, args.area, args.min,
+           args.max, 1200)
+    elif args.type == "rectangle":
+        generator = RectangleGenerator(args.number, args.area, args.min,
+           args.max, 1200)
+    
+    if args.print:  # print results
         print(generator)
 
-        if graphics:
-            generator.draw()
+    if args.graphics:
+        generator.draw()
 
-
-    except ValueError as err:
-        if err != "":
-            print(err)
-        print("Randomly generate overlapping objects in space.\n\n"
-            + "Usage:\n\tgenerator.py rectangle|interval <nr_of_objects> "
-            + "[--area=<size>] [--min=<ratio>] [--max=<ratio>] "
-            + "[-g] | --graphics\n\tgenerator.py -h | --help\n"
-            + "Options:\n\t-h --help\tShow usage\n\t"
-            + "-g --graphics\tDraw the generated objects\n\t"
-            + "<nr_of_objects>\tNumber of generated objects\n\t"
-            + "--area=<size>\tMax size of the generated area [def: 1000]\n\t"
-            + "--min=<ratio>\tMin ratio of object size/area [def: 0.01]\n\t"
-            + "--max=<ratio>\tMax ratio of object size/area [def: 0.1]\n\t")
 
 
 class Generator():
@@ -89,12 +82,13 @@ class Generator():
 
     """
 
-    def __init__(self, object_number, area_size, min_ratio, max_ratio):
+    def __init__(self, object_number, area, min_ratio, max_ratio, window):
         """Prepare space and initialize generation"""
         self.object_number = object_number
-        self.area_size = area_size
+        self.area = area
         self.min_ratio = min_ratio
         self.max_ratio = max_ratio
+        self.window = window
 
         self.objects_dict = {}
         self.objects_count = 0
@@ -104,11 +98,11 @@ class Generator():
 
     def _get_line_segment(self):
         """Get a random 1D line segment within the space boundaries"""
-        point = random.randint(0, self.area_size) # pick random 1D point
-        length = random.randint(self.area_size * self.min_ratio,
-            self.area_size * self.max_ratio) # pick random length
+        point = random.randint(0, self.area) # pick random 1D point
+        length = random.randint(self.area * self.min_ratio,
+            self.area * self.max_ratio) # pick random length
 
-        if point < self.area_size / 2: # if we're not too close to the edge
+        if point < self.area / 2: # if we"re not too close to the edge
             start = point
             end = point + length # calculate end of the interval
         else: # too close to the edge, in danger of overshooting
@@ -120,7 +114,7 @@ class Generator():
 
     def _init_graphics(self):
         """Initialize the graphics environment"""
-        self.win = GraphWin('Overlap', 1201, 1201)
+        self.win = GraphWin("Overlap", self.window + 2, self.window + 2)
 
 
     def _pause_graphics(self):
@@ -132,7 +126,7 @@ class Generator():
 
     def __repr__(self):
         """Convert to string."""
-        s = ',\n'.join(" %d: %r" % (id_, object_) # stringify dictionary
+        s = ",\n".join(" %d: %r" % (id_, object_) # stringify dictionary
             for id_, object_ in self.objects_dict.items())        
         return("Generator(objects={%s}\n,count=%d)" % (s, self.objects_count))
 
@@ -146,9 +140,9 @@ class IntervalGenerator(Generator):
         __init__ -- prepares the space and generates the intervals
         _generate_interval -- generates a single interval"""
 
-    def __init__(self, object_number, area_size, min_ratio, max_ratio):
+    def __init__(self, object_number, area, min_ratio, max_ratio, window):
         """Prepare space and initialize generation"""
-        super().__init__(object_number, area_size, min_ratio, max_ratio)
+        super().__init__(object_number, area, min_ratio, max_ratio, window)
 
         self.win = None   # controller for graphics window
 
@@ -174,6 +168,28 @@ class IntervalGenerator(Generator):
     def draw(self):
         """Draw the generated intervals"""
         self._init_graphics()
+
+        scale = self.window / self.area           # scaling to window
+        margin = self.window / self.objects_count # margin between each result
+
+        # draw each rectangle
+        for interval in self.objects_dict.values():
+            y_coord = interval.id_ * margin + 2 # display interval based on id
+
+            # calculate ends and center coordinates and adjust for window
+            start = Point(interval.start * scale, y_coord)
+            end = Point(interval.end * scale, y_coord)
+            center = Point(interval.get_center() * scale, y_coord - 10)
+
+            # create rectangle and label graphics objects
+            shape = Line(start, end)
+            shape.setWidth(3)
+            label = Text(center, interval.id_)
+
+            # draw rectangle and label graphics objects
+            shape.draw(self.win)
+            label.draw(self.win)
+
         self._pause_graphics()
 
 
@@ -188,9 +204,9 @@ class RectangleGenerator(Generator):
 
     """
 
-    def __init__(self, object_number, area_size, min_ratio, max_ratio):
+    def __init__(self, object_number, area, min_ratio, max_ratio, window):
         """Prepare space and generate rectangles"""
-        super().__init__(object_number, area_size, min_ratio, max_ratio)
+        super().__init__(object_number, area, min_ratio, max_ratio, window)
 
         self.win = None   # controller for graphics window
 
@@ -218,17 +234,16 @@ class RectangleGenerator(Generator):
         """Draw the generated rectangles"""
         self._init_graphics() # init window
 
-        scale = 1200 / self.area_size
-        # scale = 1
+        scale = self.window / self.area # scaling to window
 
         # draw each rectangle
         for rectangle in self.objects_dict.values():
             
             # retrieve corners and center coordinates and adjust for window
-            corner_1 = Point(rectangle.x1 * scale, rectangle.y1 * scale)
-            corner_2 = Point(rectangle.x2 * scale, rectangle.y2 * scale)
+            corner_1 = Point(rectangle.x1 * scale, rectangle.y1 * scale + 2)
+            corner_2 = Point(rectangle.x2 * scale, rectangle.y2 * scale + 2)
             x_center, y_center = rectangle.get_center()
-            center = Point(x_center * scale, y_center * scale)
+            center = Point(x_center * scale, y_center * scale + 2)
 
             # create rectangle and label graphics objects
             shape = Rectangle(corner_1, corner_2)
@@ -243,6 +258,6 @@ class RectangleGenerator(Generator):
 
         
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run the generator with command-line parameters.
     main()
