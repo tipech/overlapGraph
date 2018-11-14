c34bb250907b56107d0ab9899e8ab4fa37524895
Vincent Chu <vwchu@users.noreply.github.com>
2018-11-14 14:19:02 -0500

    Add tests for random_points with different dimensions

--- sources/tests/test_region.py
+++ sources/tests/test_region.py
@@ -157,11 +157,16 @@ class TestRegionNoSetup(TestCase):
           self.assertEqual(difference, None)
 
   def test_region_random_points(self):
-    region = Region([-5, 0], [15, 10])
-    points = region.random_points(5)
-    #print(f'{region}: random={points}')
-    for point in points:
-      self.assertTrue(region.contains(list(point), inc_upper=False))
+    region2d = Region([-5, 0], [15, 10])
+    region3d = Region([-5, 0, 0], [15, 10, 50])
+    points2d = region2d.random_points(5)
+    points3d = region3d.random_points(5)
+    #print(f'{region2d}: random={points2d}')
+    #print(f'{region3d}: random={points3d}')
+    for point in points2d:
+      self.assertTrue(region2d.contains(list(point), inc_upper=False))
+    for point in points3d:
+      self.assertTrue(region3d.contains(list(point), inc_upper=False))
 
   def test_interval_random_regions(self):
     region = Region([-5, 0], [15, 10])
