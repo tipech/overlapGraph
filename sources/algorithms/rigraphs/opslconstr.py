267b19724ac9fddab75a58869f1f40c5054bb291
Vincent Chu <vwchu@users.noreply.github.com>
2018-12-11 04:37:51 -0500

    Rename SweeplnAlg + SweeplnRT to OpSLEvaluator + OpSweepln
    - Rename OpslConstr to OpSLGraphConstr
    - Rename TestSweepln to TestOpSweepln
    - Move test_sweepln to test_opsweepln

--- sources/algorithms/rigraphs/opslconstr.py
+++ sources/algorithms/rigraphs/opslconstr.py
@@ -3,30 +3,30 @@
 """
 Regional Intersection Graph (RIG) Construction by One-pass Sweepline Algorithm
 
-This script implements the OpslConstr (or one-pass sweepline RIG construction
-algorithm). This algorithm builds an undirected, weighted (labelled)
+This script implements the OpSLGraphConstr (or one-pass sweep-line RIG 
+construction algorithm). This algorithm builds an undirected, labelled
 graph of all the pair-wise intersections or overlapping regions between a
 collection of regions with the same dimensionality.
 """
 
 from networkx import Graph
 
-from sources.algorithms.sweepln.opsweepln import SweeplnAlg, SweeplnRT
+from sources.algorithms.sweepln.opsweepln import OpSLEvaluator, OpSweepln
 from sources.datastructs.datasets.regionset import RegionSet
 from sources.datastructs.shapes.region import Region, RegionPair
 
 
-class OpslConstr(SweeplnAlg, SweeplnRT):
+class OpSLGraphConstr(OpSLEvaluator, OpSweepln):
   """
   Implementation of regional intersection graph construction based on a 
-  one-pass sweepline algorithm. This algorithm builds an undirected, 
+  one-pass sweep-line algorithm. This algorithm builds an undirected, 
   weighted graph of all the pair-wise intersections or overlapping
-  regions within a RegionSet. Inherits from: SweeplnAlg and SweeplnRT.
+  regions within a RegionSet. Inherits from: OpSLEvaluator and OpSweepln.
 
   Properties:         graph
   Overridden Methods:
     Special Methods:  __init__
-    SweeplnAlg:
+    OpSLEvaluator:
       Methods:        oninit, addoverlap, onfinalize
   """
   graph: Graph
@@ -34,24 +34,24 @@ class OpslConstr(SweeplnAlg, SweeplnRT):
   def __init__(self, regionset: RegionSet):
     """
     Initialize the intersection graph construction
-    by sweepline algorithm with the given RegionSet.
+    by sweep-line algorithm with the given RegionSet.
 
     :param regionset:
     """
-    SweeplnAlg.__init__(self)
-    SweeplnRT.__init__(self, regionset)
-    SweeplnRT.put(self, self)
+    OpSLEvaluator.__init__(self)
+    OpSweepln.__init__(self, regionset)
+    OpSweepln.put(self, self)
 
   def oninit(self, dimension: int):
     """
-    Initialize the evaluation of the RegionSet in the SweeplnRT
+    Initialize the evaluation of the RegionSet in the OpSweepln
     with the given dimensions. Create a new intersection Graph and
     populate it with nodes for each Region. This method extends
     the superclass implementation.
 
     :param dimension:
     """
-    SweeplnAlg.oninit(self, dimension)
+    OpSLEvaluator.oninit(self, dimension)
 
     self.graph = Graph()
 
@@ -67,7 +67,7 @@ class OpslConstr(SweeplnAlg, SweeplnRT):
     :param regionpair:
     """
     self.graph.add_edge(regionpair[0].id, regionpair[1].id, \
-                        intersect = regionpair[0].intersect(regionpair[1]))
+                        intersect=regionpair[0].intersect(regionpair[1]))
 
   def onfinalize(self) -> Graph:
     """
@@ -75,6 +75,6 @@ class OpslConstr(SweeplnAlg, SweeplnRT):
     Returns the newly constructed intersection Graph.
     This method extends the superclass implementation.
     """
-    SweeplnAlg.onfinalize(self)
+    OpSLEvaluator.onfinalize(self)
 
     return self.graph
--- sources/algorithms/sweepln/opsweepln.py
+++ sources/algorithms/sweepln/opsweepln.py
@@ -3,12 +3,12 @@
 """
 Generalized One-Pass Sweep-line Algorithm
 
-This script implements a generalized version of a single-pass
-sweepline algorithm. Implements SweeplnAlg and SweeplnRT classes, where
-the SweeplnRT (the runtime) drives the evaluation by initializing each
-SweeplnAlgs' oninit methods, looping over the sorted Events and executing
-each SweeplnAlgs' onbegin and onend handlers, and finally, invoking each
-SweeplnAlgs' onfinalize methods. The SweeplnAlg (evaluator) is a base
+This script implements a generalized version of a one-pass sweep-line 
+algorithm. Implements OpSLEvaluator and OpSweepln classes, where
+the OpSweepln (the algorithm) drives the evaluation by initializing each
+OpSLEvaluators' oninit methods, looping over the sorted Events and executing
+each OpSLEvaluators' onbegin and onend handlers, and finally, invoking each
+OpSLEvaluators' onfinalize methods. The OpSLEvaluator (evaluator) is a base
 class for implementations that implement these handlers and maintain the
 necessary, associated interval state of the evaluation.
 """
@@ -20,11 +20,11 @@ from sources.datastructs.datasets.timeline import Event, EventKind
 from sources.datastructs.shapes.region import Region, RegionPair
 
 
-class SweeplnAlg:
+class OpSLEvaluator:
   """
-  Base class for implementing a SweeplnAlg (evaluator) that is
-  binded to and evaluated when the SweeplnRT evaluates a single-pass,
-  sweepline along a dimension on a set of Regions.
+  Base class for implementing a OpSLEvaluator (evaluator) that is
+  binded to and evaluated when the OpSweepln evaluates a one-pass,
+  sweep-line along a dimension on a set of Regions.
 
   Properties:           runtime, regionset, dimension, actives, overlaps
   Computed Properties:  binded, initialized
@@ -32,14 +32,14 @@ class SweeplnAlg:
   Methods:              bind, unbind, findoverlaps, addoverlap,
                         oninit, onbegin, onend, onfinalize
   """
-  runtime   : 'SweeplnRT'
+  runtime   : 'OpSweepln'
   regionset : RegionSet
   dimension : int
   actives   : Dict[str, Region]
   overlaps  : List[RegionPair]
 
   def __init__(self):
-    """Initialize this SweeplnAlg with None values."""
+    """Initialize this OpSLEvaluator with None values."""
     self.runtime = None
     self.regionset = None
     self.dimension = None
@@ -47,24 +47,24 @@ class SweeplnAlg:
   @property
   def binded(self) -> bool:
     """
-    Determine if this SweeplnAlg is binded to a SweeplnRT.
+    Determine if this OpSLEvaluator is binded to a OpSweepln.
     Return True if it is binded otherwise False.
     """
-    return isinstance(self.runtime, SweeplnRT)
+    return isinstance(self.runtime, OpSweepln)
 
   @property
   def initialized(self) -> bool:
     """
-    Determine if this SweeplnAlg has been initialized for evaluation.
+    Determine if this OpSLEvaluator has been initialized for evaluation.
     Return True if it is initialized otherwise False.
     """
     return isinstance(self.dimension, int)
 
-  def bind(self, runtime: 'SweeplnRT', unbind: bool = False):
+  def bind(self, runtime: 'OpSweepln', unbind: bool = False):
     """
-    Bind or attach this SweeplnAlg to the given SweeplnRT.
-    If the unbind flag is True, unbinds the previous SweeplnRT if this
-    SweeplnAlg is currently binded to it.
+    Bind or attach this OpSLEvaluator to the given OpSweepln.
+    If the unbind flag is True, unbinds the previous OpSweepln if this
+    OpSLEvaluator is currently binded to it.
 
     :param runtime:
     :param unbind:
@@ -79,8 +79,8 @@ class SweeplnAlg:
 
   def unbind(self):
     """
-    Unbind or detach this SweeplnAlg from its currently 
-    atteched SweeplnRT.
+    Unbind or detach this OpSLEvaluator from its currently 
+    atteched OpSweepln.
     """
     if self.binded:
       self.runtime = None
@@ -118,7 +118,7 @@ class SweeplnAlg:
 
   def oninit(self, dimension: int):
     """
-    Initialize the evaluation of the RegionSet in the SweeplnRT
+    Initialize the evaluation of the RegionSet in the OpSweepln
     with the given dimensions. This method should be overridden in
     subclasses to implement:
 
@@ -137,7 +137,7 @@ class SweeplnAlg:
 
   def onbegin(self, event: Event):
     """
-    When a Begin Event is encountered in the SweeplnRT evaluation, this
+    When a Begin Event is encountered in the OpSweepln evaluation, this
     method is invoked with that Event. Invokes findoverlaps and 
     addoverlap methods from here. Adds the newly active Region to the
     set of active Regions.
@@ -156,7 +156,7 @@ class SweeplnAlg:
 
   def onend(self, event: Event):
     """
-    When an End Event is encountered in the SweeplnRT evaluation, this
+    When an End Event is encountered in the OpSweepln evaluation, this
     method is invoked with that Event. Removes the ending Region from
     to the set of active Regions.
 
@@ -171,16 +171,16 @@ class SweeplnAlg:
 
   def onfinalize(self) -> List[RegionPair]:
     """
-    When the SweeplnRT evaluation is complete, the sweep is complete,
-    this method is invoked. Uninitialized this SweeplnAlg. Returns
-    the result of the sweepline algorithm; for this base implementation
+    When the OpSweepln evaluation is complete, the sweep is complete,
+    this method is invoked. Uninitialized this OpSLEvaluator. Returns
+    the result of the sweep-line algorithm; for this base implementation
     returns the list of Region overlapping pairs.
 
     This method should be overridden in subclasses to implement any
     cleanup or finalization routines for data structures, such as
     intersection graphs and interval trees, or initialize the next
     pass on a different dimension for multiple pass versions of the
-    sweepline algorithm. Should return references to the generated
+    sweep-line algorithm. Should return references to the generated
     intersection graphs and interval trees.
     """
     assert self.binded and self.initialized
@@ -190,36 +190,36 @@ class SweeplnAlg:
     return self.overlaps
 
 
-class SweeplnRT:
+class OpSweepln:
   """
-  Runtime for the generalized single-pass sweepline algorithm.
+  Runtime for the generalized one-pass sweep-line algorithm.
   This class implements the sorting and determining the overlapping
-  Regions along a specified dimension via one or more SweeplnAlg
-  instances. Loops over that event generated by the RegionSet's
-  timeline, calls the onbegin or onend methods of the SweeplnAlgs
-  depending on EventKind.
+  Regions along a specified dimension via one or more OpSLEvaluators.
+  Loops over that event generated by the RegionSet's Timeline, calls
+  the onbegin or onend methods of the OpSLEvaluators depending on
+  EventKind.
 
   Properties:       regionset, evaluators
   Special Methods:  __init__
   Methods:          put, evaluate
   """
   regionset: RegionSet
-  evaluators: List[SweeplnAlg]
+  evaluators: List[OpSLEvaluator]
 
   def __init__(self, regionset: RegionSet):
     """
-    Initialize the sweepline runtime with the given RegionSet
-    and an empty list of SweeplnAlgs.
+    Initialize the sweep-line runtime with the given RegionSet
+    and an empty list of OpSLEvaluators.
 
     :param regionset:
     """
     self.regionset = regionset
     self.evaluators = []
 
-  def put(self, evaluator: SweeplnAlg):
+  def put(self, evaluator: OpSLEvaluator):
     """
-    Adds the given SweeplnAlg to the list of evaluators for
-    this sweepline algorithm Runtime to bind and execute
+    Adds the given OpSLEvaluator to the list of evaluators for
+    this sweep-line algorithm Runtime to bind and execute
     when evaluating the input Regions.
 
     :param evaluator:
@@ -232,11 +232,11 @@ class SweeplnRT:
 
   def evaluate(self, dimension: int) -> List[Any]:
     """
-    Execute the sweepline algorithm on the set of Regions
-    along the given dimension. Invokes the SweeplnAlg at the initialization phase,
+    Execute the sweep-line algorithm on the set of Regions
+    along the given dimension. Invokes the OpSLEvaluator at the initialization phase,
     when encountering the beginning of an overlap and ending of an overlap Events,
     and at the finalization phase of the algorithm. Returns a list of results, one
-    result for each SweeplnAlg.
+    result for each OpSLEvaluator.
 
     :param dimension:
     """
