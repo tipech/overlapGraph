577570df7c0d704eee52a74c9b1d969326550d34
Vincent Chu <vwchu@users.noreply.github.com>
2018-12-25 11:58:02 -0500

    Add to_object + from_object to NxGraph + unit tests
    - Add unit test for NxGraphSweepCtor

--- sources/datastructs/rigraphs/nxgraph.py
+++ sources/datastructs/rigraphs/nxgraph.py
@@ -24,9 +24,10 @@ Classes:
 - NxGraph
 """
 
-from typing import Any, Generic, Iterator, Tuple, TypeVar
+from typing import Any, Callable, Dict, Generic, Iterator, List, Tuple, TypeVar
 
 from networkx import networkx as nx
+from networkx.readwrite import json_graph
 
 from sources.datastructs.datasets.ioable import IOable
 from sources.datastructs.rigraphs.rigraph import RIGraph
@@ -154,3 +155,125 @@ class NxGraph(RIGraph[nx.Graph], IOable):
 
     self.G.add_edge(overlap[0].id, overlap[1].id, \
                     intersect=overlap[0].intersect(overlap[1], 'reference'))
+
+  ### Class Methods: Serialization
+
+  @classmethod
+  def to_object(cls, object: 'NxGraph', format: str = 'json', **kwargs) -> Any:
+    """
+    Generates an object (dict, list, or tuple) from the given NxGraph object
+    that can be converted or serialized.
+
+    Args:
+      object:   The NxGraph object to be converted to an
+                object (dict, list, or tuple).
+      format:   The output serialization format: 'json'.
+      kwargs:   Additional arguments to be used to
+                customize and tweak the object generation
+                process.
+
+    kwargs:
+      json_graph:
+        Allowed graph formats: 'node_link' or 'adjacency'.
+        If not provided, defaults to: 'node_link'.
+
+    Returns:
+      The generated object (dict, list, or tuple).
+
+    Raises:
+      ValueError: If json_graph is unsupported format.
+    """
+    assert isinstance(object, NxGraph) and isinstance(object.G, nx.Graph)
+    assert isinstance(object.dimension, int) and object.dimension > 0
+
+    def to_data(G, datafmt):
+      method_name = f'{datafmt}_data'
+      if hasattr(json_graph, method_name):
+        method = getattr(json_graph, method_name)
+        assert isinstance(method, Callable)
+        return method(G)
+      else:
+        raise ValueError(f'Unsupported json_graph format.')
+
+    datafmt = kwargs['json_graph'] if 'json_graph' in kwargs else 'node-link'
+    data = {
+      'dimension': object.dimension,
+      'json_graph': datafmt,
+      'graph': to_data(object.G, datafmt)
+    }
+
+    return data
+
+  ### Class Methods: Deserialization
+
+  @classmethod
+  def from_object(cls, object: Any, **kwargs) -> 'NxGraph':
+    """
+    Construct a new NxGraph object from the conversion of the given object.
+
+    Args:
+      object:   The object (dict, list, or tuple)
+                to be converted into an NxGraph object.
+      kwargs:   Additional arguments for customizing
+                and tweaking the NxGraph object
+                generation process.
+
+    kwargs:
+      json_graph:
+        Allowed graph formats: 'node_link' or 'adjacency'.
+        If not provided, defaults to: 'node_link'.
+
+    Returns:
+      The newly constructed NxGraph object.
+
+    Raises:
+      ValueError: If json_graph is unsupported format.
+    """
+    types = { 'graph': Dict, 'json_graph': str, 'dimension': int }
+
+    assert isinstance(object, Dict)
+    assert all([k in object and isinstance(object[k], t) for k, t in types.items()])
+    assert object['dimension'] > 0
+
+    def to_graph(data: Dict, datafmt: str) -> nx.Graph:
+      method_name = f'{datafmt}_graph'
+      if hasattr(json_graph, method_name):
+        method = getattr(json_graph, method_name)
+        assert isinstance(method, Callable)
+        return method(data)
+      else:
+        raise ValueError(f'Unsupported json_graph format.')
+
+    def to_regions(G: nx.Graph, regions: List[str], edge: Tuple[str, str]) -> List[Region]:
+      assert isinstance(regions, List) and len(regions) >= 2
+      assert all([isinstance(r, str) for r in regions])
+      assert all([r in regions for r in edge])
+      assert all([r in G.node for r in regions])
+
+      return list(map(lambda r: G.node[r]['region'], regions))
+
+    nxgraph = NxGraph(object['dimension'])
+    nxgraph.G = to_graph(object['graph'], object['json_graph'])
+    G = nxgraph.G
+
+    assert nxgraph.dimension == G.graph['dimension']
+
+    # Rematerialize Regions
+    for node, region_data in G.nodes(data='region'):
+      region = Region.from_object(region_data)
+      assert region.dimension == nxgraph.dimension
+      G.node[node]['region'] = region
+
+    # Resolve backlinks amongst the edge, Region intersections
+    for (u, v, region_data) in G.edges(data='intersect'):
+      region = Region.from_object(region_data)
+      assert region.dimension == nxgraph.dimension
+
+      if 'intersect' in region.data:
+        region.data['intersect'] = to_regions(G, region.data['intersect'], (u, v))
+      if 'union' in region.data:
+        region.data['union'] = to_regions(G, region.data['union'], (u, v))
+
+      G.edges[u, v]['intersect'] = region
+
+    return nxgraph
---
+++ sources/tests/test_nxgraph.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+"""
+Unit tests for Regional Intersection Graph -- NetworkX
+
+This script implements the following tests:
+  - test_nxgraph_create
+  - test_nxgraph_sweepctor
+  - test_nxgraph_sweepctor_graph
+"""
+
+from io import StringIO
+from typing import List, Tuple
+from unittest import TestCase
+
+from sources.algorithms.rigctor.nxgsweepctor import NxGraphSweepCtor
+from sources.datastructs.datasets.regionset import RegionSet
+from sources.datastructs.rigraphs.nxgraph import NxGraph
+from sources.datastructs.shapes.region import Region, RegionPair
+
+
+def reset_output(output: StringIO) -> StringIO:
+  output.seek(0)
+  return output
+
+
+class TestNxGraph(TestCase):
+
+  test_regions: List[Region]
+
+  def setUp(self):
+    self.test_regions = []
+    self.test_regions.append(Region([0, 0], [5, 5]))
+    self.test_regions.append(Region([2, 2], [5, 10]))
+    self.test_regions.append(Region([1, 5], [3, 7]))
+    self.test_regions.append(Region([-5, 5], [1, 7]))
+    self.test_regions.append(Region([-5, 5], [2, 7]))
+
+  def naive_ctor(self, nxgraph: NxGraph):
+    for region in self.test_regions:
+      nxgraph.put_region(region)
+
+    for region in self.test_regions:
+      for that in self.test_regions:
+        if region is that:
+          continue
+        if region.overlaps(that):
+          nxgraph.put_overlap((region, that))
+
+  def check_nxgraph(self, a: NxGraph, b: NxGraph):
+    self.assertEqual(a.dimension, b.dimension)
+    for anode, aregion in a.G.nodes(data='region'):
+      self.assertTrue(anode in b.G.node)
+      self.assertTrue(isinstance(aregion, Region))
+      self.assertTrue(isinstance(b.G.node[anode]['region'], Region))
+      self.assertEqual(aregion, b.G.node[anode]['region'])
+
+    for (u, v, aregion) in a.G.edges(data='intersect'):
+      self.assertTrue((u, v) in b.G.edges)
+      bregion = b.G.edges[u, v]['intersect']
+      #print(f'A: {aregion}: {aregion["intersect"]}')
+      #print(f'B: {bregion}: {bregion["intersect"]}')
+      self.assertTrue(isinstance(aregion, Region))
+      self.assertTrue(isinstance(bregion, Region))
+      self.assertEqual(aregion, bregion)
+      if 'intersect' in aregion.data:
+        self.assertTrue('intersect' in bregion.data)
+        self.assertTrue(all([isinstance(r, Region) for r in aregion['intersect']]))
+        self.assertTrue(all([isinstance(r, Region) for r in bregion['intersect']]))
+        self.assertTrue(all([r in bregion['intersect'] for r in aregion['intersect']]))
+
+  def test_nxgraph_create(self):
+    dimension = self.test_regions[0].dimension
+    nxgraph = NxGraph(dimension=dimension)
+    self.naive_ctor(nxgraph)
+
+    with StringIO() as output:
+      for json_graph in ['node_link', 'adjacency']:
+        options = { 'json_graph': json_graph, 'compact': True }
+        output.truncate(0)
+        nxgraph.to_output(reset_output(output), options=options)
+        #json_output = output.getvalue()
+        #print(f'{json_graph}:')
+        #print(f'{json_output}')
+        newgraph = NxGraph.from_source(reset_output(output))
+        self.check_nxgraph(nxgraph, newgraph)
+
+  def test_nxgraph_sweepctor(self):
+    dimension = self.test_regions[0].dimension
+    regionset = RegionSet(dimension=dimension)
+    regionset.streamadd(self.test_regions)
+    nxgraphctor = NxGraphSweepCtor(regionset, 'nxgraphctor')
+    nxgraphctor.evaluate(dimension=0)
+    nxgraph = nxgraphctor['nxgraphctor']
+
+    with StringIO() as output:
+      for json_graph in ['node_link', 'adjacency']:
+        options = { 'json_graph': json_graph, 'compact': True }
+        output.truncate(0)
+        nxgraph.to_output(reset_output(output), options=options)
+        #json_output = output.getvalue()
+        #print(f'{json_graph}:')
+        #print(f'{json_output}')
+        newgraph = NxGraph.from_source(reset_output(output))
+        self.check_nxgraph(nxgraph, newgraph)
+
+  def test_nxgraph_sweepctor_graph(self):
+    dimension = self.test_regions[0].dimension
+    nxgraph = NxGraph(dimension=dimension)
+    self.naive_ctor(nxgraph)
+
+    regionset = RegionSet(dimension=dimension)
+    regionset.streamadd(self.test_regions)
+    nxgraphctor = NxGraphSweepCtor(regionset, 'nxgraphctor')
+    nxgraphctor.evaluate(dimension=0)
+    nxgraphsweepln = nxgraphctor['nxgraphctor']
+
+    G = nxgraph.G
+    S = nxgraphsweepln.G
+
+    self.assertEqual(nxgraph.dimension, nxgraphsweepln.dimension)
+    for node, region in G.nodes(data='region'):
+      self.assertTrue(node in S.node)
+      self.assertEqual(region, S.node[node]['region'])
+
+    for (u, v, aregion) in G.edges(data='intersect'):
+      self.assertTrue((u, v) in S.edges)
+      bregion = S.edges[u, v]['intersect']
+      self.assertEqual(aregion, bregion)
+      self.assertTrue('intersect' in aregion.data)
+      self.assertTrue('intersect' in bregion.data)
+      aintersect = aregion['intersect']
+      bintersect = bregion['intersect']
+      self.assertTrue(all([r in bintersect for r in aintersect]))
