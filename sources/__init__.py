1074dd7b1fefef7b5c9fb183d2dd233a53e2bc15
Vincent Chu <vwchu@users.noreply.github.com>
2019-01-13 19:38:42 -0500

    Rename commands package to console

--- sources/__init__.py
+++ sources/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 from sources.algorithms import *
-from sources.commands import *
+from sources.console import *
 from sources.datastructs import *
 from sources.experiments import *
 from sources.helpers import *
